3851ac61170c1d4f9130cb7afc28a23676db033d
itai zeitak <itai12312@users.noreply.github.com>
2016-04-13 00:38:34 +0300

    Delete modXlevel2.py

--- pyprograms/hackthissiteuleretc/modXlevel2.py
+++
@@ -1,8 +0,0 @@
-string=open("level2.txt","r").read()
-for off in [25,26,27,28,29,59]:
-    print "\n\nwith offset",off
-    decoded=""
-    for c in string:
-        if off+ord(c) in range(32,127):
-            decoded+=chr(off+ord(c))
-    print decoded
